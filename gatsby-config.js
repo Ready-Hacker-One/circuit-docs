66b744ec3be75d03f398565e85434db7bb04146b
Cristian Oliveira <contato@cristianoliveira.com.br>
2018-02-06 16:51:57 +0100

    Add circuit-ui as dependecy and react-docgen integration
      closes #2

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-react-docgen',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'documents',
+        path: `${__dirname}/node_modules/circuit-ui/src/components`,
+      },
+    },
+  ],
 };
